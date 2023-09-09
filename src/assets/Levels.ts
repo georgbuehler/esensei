98edca6269de92cb977ed3ba7d6f23207f0fd395
georgbuehler <46175084+georgbuehler@users.noreply.github.com>
2023-09-09 14:43:35 -0400

    Added static data files for levels, techniques.

---
+++ src/assets/Levels.ts
@@ -0,0 +1,18 @@
+export interface Level {
+  Id: number;
+  Name: string;
+  EnglishTranslation: string;
+}
+
+const levels = [
+  { Id: 5, Name: "Gokyu", EnglishTranslation: "Fifth Kyu" },
+  { Id: 4, Name: "Yonkyu", EnglishTranslation: "Fourth Kyu" },
+  { Id: 3, Name: "Sankyu", EnglishTranslation: "Third Kyu" },
+  { Id: 2, Name: "Nikkyu", EnglishTranslation: "Second Kyu" },
+  { Id: 1, Name: "Ikkyu", EnglishTranslation: "First Kyu" },
+  { Id: 0, Name: "Shodan", EnglishTranslation: "First Dan" },
+  { Id: -1, Name: "Nidan", EnglishTranslation: "Second Dan" },
+  { Id: -2, Name: "Sandan", EnglishTranslation: "Third Dan" },
+];
+
+export default levels;
